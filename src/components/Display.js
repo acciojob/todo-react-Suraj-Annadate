0aec658ac159dd385e663b6376be5a08fcb84698
suraj0698 <annadatesuraj@gmail.com>
2023-08-25 17:02:22 +0530

    c1

---
+++ src/components/Display.js
@@ -0,0 +1,20 @@
+import React from "react"
+
+const Display=({listdata,setListdata})=>{
+
+    function removefunc (e,index) {
+     console.log("hi",index);
+        let newlist =listdata.filter((ele,ind)=>{return (index!==ind)})
+        setListdata([...newlist])
+    }
+
+    return(<>
+    <ul>
+        {listdata.map((elem,index)=>{ return (<li key={index}>
+            <span>{elem}</span> <span><button onClick={(e)=>{removefunc(e,index)}}>Delete</button></span>
+            </li>)})}
+            </ul>
+        </>)
+}
+
+export default Display;
