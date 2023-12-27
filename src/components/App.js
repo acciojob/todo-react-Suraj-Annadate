f5899f965d591b061266a6a0b9b7c2cc5144d1cf
suraj0698 <annadatesuraj@gmail.com>
2023-12-27 12:00:08 +0530

    c3

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,52 @@
 
-import React, { useState } from "react";
+import React,{useState} from "react";
 import './../styles/App.css';
 
-  const App = () =>{
-        
-      let [list,setList] = useState([]);
-      let [item,setItem] = useState("");
-      
-        function addingItem(e){
-            setItem(e.target.value);
-        }
 
-        function addtolist(){
-              setList([...list,item]); 
-              setItem('');      
-        }
+const App = () => {
+  let [values,setvalue]=useState("");
+  let [AllTodoValues,setAllTodoValues]=useState([]);
+  let [dynamicId,setdynamicId]=useState(1);
 
-        function removeitem(itemName){
-          const updatedTodoList = list.filter((item)=>item !== itemName);
-               setList(updatedTodoList);
-        }
-
-    return (
-        <div>
-          <div>
-              <h1>To-Do List</h1>
-              <input type="text" onChange={addingItem} value={item}/>
-              <button onClick={addtolist}>Add Todo</button>
-          </div>
-          {
-            list && list.map((items,index)=>(
-                <div key={index}>
-                    <span className="itemName">{items}</span>
-                     {console.log(items)}
-                    <button onClick={() =>removeitem(items)}>Delete</button>
-                </div>
-              ))
-          }
-        </div>
-    )
+  function enterTodo(e){
+    setvalue(e.target.value);
   }
-  export default App;
+  function AddTodo(e)
+  {
+       e.preventDefault();
+      setAllTodoValues([...AllTodoValues,{id:dynamicId,tittle:values}]);
+      setdynamicId(dynamicId+1);
+      setvalue("");
+  }
+
+  function deleteitem(id)
+  {
+       setAllTodoValues(AllTodoValues.filter((item)=>{
+          if(item.id!=id)
+          {
+            return item;
+          }
+       }))
+  }
+  return (
+    
+    <div>
+        <form onSubmit={AddTodo}>
+          <input type="text" placeholder="enter todo" onChange={enterTodo} value={values}/>
+          <button type="submit">Add Task</button>
+        </form>
+        <ul>
+         {
+           AllTodoValues.map((item)=>{
+            return <li>
+              <p>{item.tittle}</p>
+              <button onClick={()=>{deleteitem(item.id)}}>Delete</button>
+            </li>
+           })
+         }
+        </ul>
+    </div>
+  )
+}
+
+export default App
